5fe075cb6683c937b68d25e3fedaae89e7345769
Anthony Cortese <akcortese@Anthonys-MacBook-Pro.local>
2021-01-20 16:52:22 -0700

    completed project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios"
+import Header from "./Components/Header"
+import { BASE_URL, API_KEY } from "./Constants/index"
 
 function App() {
+  const [photo, setPhoto] = useState({});
+  useEffect(() => {
+    const getPic = () => {
+      axios
+      .get(`${BASE_URL}?api_key=${API_KEY}`)
+      .then(res => {
+        setPhoto(res.data);
+      }) 
+      .catch(err => {
+
+      })
+    }
+    getPic()
+  }, []);
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
-    </div>
+
+ <Header /> 
+<>
+ <img src={photo.url}/>
+  <h2>{photo.title}</h2>
+  <h3>{photo.copyright}</h3>
+  <h4>{photo.date}</h4>
+  <p>{photo.explanation}</p>  
+</>
   );
-}
+
 
 export default App;
---
+++ src/Constants/index.js
@@ -0,0 +1,2 @@
+// export const BASE_URL = https://localhost:3000/
+// export const API_KEY = HGH9oEIYxqN9P4OVgehJxo615IP7DtbJonHROse5
